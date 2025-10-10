d820608f8ad8a9bebaf4072db1f12a9800bba264
Sky28702 <167354173+Sky28702@users.noreply.github.com>
2025-10-11 00:11:07 +0530

    searchbox design fix

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,8 +2,8 @@ import { IconSearch } from "@tabler/icons-react";
 function Search() {
   return (
     <section className="text-black p-4">
-      <form className=" flex flex-row gap-6">
-        <div className="  border  flex items-center bg-white border-black w-full rounded-4xl">
+      <form className=" flex flex-row justify-between">
+        <div className="  border  flex items-center bg-white border-black w-100 rounded-4xl">
           <span className="bg-white rounded-bl-4xl rounded-tl-4xl p-4">
             <IconSearch stroke={2} />
           </span>
@@ -14,7 +14,7 @@ function Search() {
           />
         </div>
         <div className=" flex items-center">
-          <select className="bg-white rounded-[4px] h-12 outline-none p-1">
+          <select className="bg-white rounded-[4px] w-40 h-12 outline-none p-1">
             <option>Sort By</option>
             <option value="A-Z">A-Z</option>
             <option value="Release Date">Release Date</option>
